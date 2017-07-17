bd2615fa995c572f43398aa3d04c7ccfcc5aa717
Brooks Swinnerton <bswinnerton@gmail.com>
2017-07-16 22:15:00 -0400

    Add /download and /install links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/download', to: redirect(ENV.fetch('DOWNLOAD_URL'))
+  get '/install', to: redirect('/auth/github')
   get '/auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
+  get '/auth/failure', to: redirect('/')
 end
